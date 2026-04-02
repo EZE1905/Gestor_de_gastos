c139f797b9832b058ff9ebacbf556b4551d37577
Ezequiel Albornoz <eze_thomi@hotmail.com>
2026-04-01 21:41:05 -0300

    mejoras en el menu, opcion de eliminar gasto y ver gastos agregada

--- m_gestor_de_gastos.py
+++ m_gestor_de_gastos.py
@@ -16,28 +16,58 @@ def menu():
     print("")
     print("-----MENU-----")
     print("1. Agregar gasto")
-    print("2. Mostrar gastos")
-    print("3. Salir")
+    print("2. Eliminar gasto")
+    print("3. Mostrar gastos")
+    print("4. Salir")
     print("")
     
 def agregar_gasto(gastos):
     while True: 
         try:
             monto = int(input("ingrese el monto: "))
-            categoria = input("ingrese una categoria: ").lower()
+            categoria = input("ingrese una categoria: ").capitalize()
             ahora = datetime.now()
             #agregando la fecha del ingreso del gasto nuevo
             fecha = ahora.strftime("%d/%m/%Y")
             gasto_nuevo = {
-                "Monto" : f"$ {monto}",
+                "Monto" : monto,
                 "Categoria" : f"{categoria}",
                 "Fecha" : f"{fecha}"
             }
             gastos.append(gasto_nuevo)
             with open ("gastos.json", "w") as archivo:
-                json.dump(gastos,archivo) 
+                json.dump(gastos,archivo,indent=4) 
                 print("")
                 print("GASTO AGREGADO CORRECTAMENTE")
             break
         except Exception:
+            print("")
             print("ERROR INTENTE DE NUEVO")
+            print("")
+
+def mostrar_gastos(gastos):
+    if len(gastos) > 0:
+        print("-----GASTOS-----")
+        for i,gasto in enumerate(gastos):
+            print(f"{i + 1}. ${gasto["Monto"]} {gasto["Categoria"]} {gasto["Fecha"]}")
+        print("-----------------")
+    else: 
+        print("No hay gastos que mostrar")
+
+def eliminar_gasto(gastos):
+        mostrar_gastos(gastos)
+        try:
+            gasto_eliminado = int(input("Ingrese el gasto que desea eliminar: "))
+            if gasto_eliminado > len(gastos) - 1:
+                print("Opcion inexistente")
+            else:
+                eliminacion = gastos.pop(gasto_eliminado - 1)
+                print(f"Se elimino: ${eliminacion ["Monto"]} {eliminacion["Categoria"]} {eliminacion["Fecha"]}")
+                with open ("gastos.json", "w") as archivo:
+                    json.dump(gastos,archivo,indent=4) 
+                    print("GASTO ELIMINADO CORRECTAMENTE")
+            return gastos
+        except Exception:
+            print("")
+            print("ERROR INTENTE DE NUEVO")
+            print("")
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from m_gestor_de_gastos import leer_gastos,menu,agregar_gasto
+from m_gestor_de_gastos import leer_gastos,menu,agregar_gasto,mostrar_gastos,eliminar_gasto
 import json
 
 gastos = leer_gastos()
@@ -7,13 +7,20 @@ while True:
     try:
         eleccion = int(input("Ingrese una opcion: "))
         print("")
+        if eleccion not in [1,2,3,4]:
+            print("Opcion inexistente")
+        elif eleccion == 1:
+            agregar_gasto(gastos)
+        elif eleccion == 2:
+            if len(gastos) > 0:
+                eliminar_gasto(gastos)
+            else:
+                print("No hay gastos que eliminar")
+        elif eleccion == 3:
+            mostrar_gastos(gastos)
+        elif eleccion == 4:
+            break
     except Exception:
+        print("")
         print("Elija una de las opciones.")
-    if eleccion not in [1,2,3]:
-        print("Opcion inexistente")
-    elif eleccion == 1:
-        agregar_gasto(gastos)
-    elif eleccion == 2:
-        print(gastos)
-    elif eleccion == 3:
-        break
+        print("")
