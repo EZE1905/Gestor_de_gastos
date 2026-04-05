133d95194335b260abd5065b67ba4358bcd6d1fb
Ezequiel Albornoz <eze_thomi@hotmail.com>
2026-04-04 21:47:44 -0300

    configuracion y enrutamiento de la app

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template,redirect,url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hola Flask!"
+    #return "Hola Flask!"
+    return render_template('index.html')
+
+@app.route("/agregar_datos")
+def agregar():
+    return render_template('agregar.html')
+
+def pagina_no_encontrada(error):
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
+    app.register_error_handler(404,pagina_no_encontrada)
+    #debug es el modo de depuracion activo que significa que muestra los cambios cuando guardas
     app.run(debug=True)
