09b3ee259bd7389978af59aa19961188e77c48bc
Ezequiel Albornoz <eze_thomi@hotmail.com>
2026-04-12 21:51:41 -0300

    Estilos en el toast

--- static/js/layout.js
+++ static/js/layout.js
@@ -5,9 +5,14 @@ const contenedor = document.getElementById("contenedor_msj")
 mensajes.forEach(mensaje => {
             const nuevoDiv = document.createElement("div");
             nuevoDiv.textContent = mensaje[1]
-            nuevoDiv.classList.add(mensaje[0])
+            nuevoDiv.classList.add(mensaje[0],"contenedor_msj")
             contenedor.appendChild(nuevoDiv)
-            setTimeout(()=>{
-                nuevoDiv.remove()
-            },3000)
+            setTimeout(() => {
+                nuevoDiv.style.opacity = "0";
+                nuevoDiv.style.transform = "translateX(30px)";
+
+                setTimeout(() => {
+                    nuevoDiv.remove();
+                }, 500);
+}, 3000);
         })
